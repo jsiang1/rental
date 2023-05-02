81f05118a4a3a7266f7d6567bcf0b7bcd07062d3
jsiang1 <100522161+jsiang1@users.noreply.github.com>
2023-05-02 20:02:55 +0800

    Update RenApp.py

--- RenApp.py
+++ RenApp.py
@@ -41,7 +41,7 @@ def AddEmp():
     ans = request.form['ans']
     ren_image_file = request.files['ren_image_file']
 
-    insert_sql = "INSERT INTO rental VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO rental VALUES (%s, %s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     if ren_image_file.filename == "":
